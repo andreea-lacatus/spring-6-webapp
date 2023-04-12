88a0f4943852efa0be80917133b7149a465da56a
ACCESA\andreea.lacatus <andreea.lacatus@accesa.eu>
2023-04-12 15:20:12 +0300

    Add Publisher Entity and relationships

--- src/main/java/com/springboot/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/springboot/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package com.springboot.spring6webapp.bootstrap;
 
 import com.springboot.spring6webapp.domain.Author;
 import com.springboot.spring6webapp.domain.Book;
+import com.springboot.spring6webapp.domain.Publisher;
 import com.springboot.spring6webapp.repositories.AuthorRepository;
 import com.springboot.spring6webapp.repositories.BookRepository;
+import com.springboot.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -27,12 +31,22 @@ public class BootstrapData implements CommandLineRunner {
         Book book = new Book();
         book.setTitle("Domain driven design");
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("HarperCollins");
+        publisher.setCity("New York");
+
+        Publisher savedPublisher = publisherRepository.save(publisher);
         Author savedAuthor = authorRepository.save(author);
         Book savedBook = bookRepository.save(book);
 
         savedAuthor.getBooks().add(savedBook);
+        savedBook.setPublisher(savedPublisher);
+
+        authorRepository.save(savedAuthor);
+        bookRepository.save(savedBook);
 
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
     }
 }
